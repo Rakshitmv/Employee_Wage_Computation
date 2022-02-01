e807f24bc1a4547152f67a1c0bbdd4e1f47be3a7
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-01 15:22:26 +0530

    [Rakshit] Add: UC5 Employee monthly wages

--- EmployeeWageComputation/WageComputation.cs
+++ EmployeeWageComputation/WageComputation.cs
@@ -10,24 +10,27 @@ namespace EmployeeWageComputation
     {
         public void CalculateEmployeeWage()
         {
-            const int PART_TIME = 0, FULL_TIME = 1, ABSENT = 0, WAGE_PER_HOUR = 20, FULL_DAY_HOUR = 8, PART_TIME_HOUR = 4;
-            int dailyWage = 0;
+            const int PART_TIME = 0, FULL_TIME = 1, WAGE_PER_HOUR = 20, FULL_DAY_HOUR = 8, PART_TIME_HOUR = 4, WORKING_DAYS = 20;
+            int monthlyWage = 0;
             Random random = new Random();
-            int check = random.Next(0, 3);
-            switch (check)
+            for (int i = 0; i < WORKING_DAYS; i++)
             {
-                case PART_TIME:
-                    dailyWage += WAGE_PER_HOUR * PART_TIME_HOUR;
-                    break;
-                case FULL_TIME:
-                    dailyWage += WAGE_PER_HOUR * FULL_DAY_HOUR;
-                    break;
-                default:
-                    dailyWage = 0;
-                    break;
+                int check = random.Next(0, 3);
+                switch (check)
+                {
+                    case PART_TIME:
+                        monthlyWage += WAGE_PER_HOUR * PART_TIME_HOUR;
+                        break;
+                    case FULL_TIME:
+                        monthlyWage += WAGE_PER_HOUR * FULL_DAY_HOUR;
+                        break;
+                    default:
+                        monthlyWage += 0;
+                        break;
+                }
             }
+            Console.WriteLine("The Monthly Wage Of the Employee is :{0}", monthlyWage);
 
-            Console.WriteLine("The today Wage Of the Employee is :{0}", dailyWage);
 
 
         }
