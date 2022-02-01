a940548cd2d87ac7c74bab92f55eee1012dbfc1a
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-01 14:38:23 +0530

    [Rakshit] Add: UC1 Emp attendance

---
+++ EmployeeWageComputation/WageComputation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class WageComputation
+    {
+        public void Attendance()
+        {
+            const int PRESENT = 1, ABSENT = 0;
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == PRESENT)
+            {
+                Console.WriteLine("Employee is Present \n");
+            }
+            if (check == ABSENT)
+            {
+                Console.WriteLine("Employee is Absent \n");
+            }
+        }
+    }
+}
