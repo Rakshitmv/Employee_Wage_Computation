75124f842b57ec2bc9809b39a0e69dd9b56b7b3c
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-01 14:44:11 +0530

    [Rakshit] Add: UC2 Employee daily wage

--- EmployeeWageComputation/WageComputation.cs
+++ EmployeeWageComputation/WageComputation.cs
@@ -10,12 +10,14 @@ namespace EmployeeWageComputation
     {
         public void Attendance()
         {
-            const int PRESENT = 1, ABSENT = 0;
+            const int PRESENT = 1, ABSENT = 0, WAGE_PER_HOUR = 20, FULL_DAY_HOUR = 8;
             Random random = new Random();
             int check = random.Next(0, 2);
             if (check == PRESENT)
             {
                 Console.WriteLine("Employee is Present \n");
+                int dailywage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                Console.WriteLine("The Daily Wage Of the Employee is : {0}", dailywage);
             }
             if (check == ABSENT)
             {
